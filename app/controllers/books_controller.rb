cb9222204667e7bfa0cecfbab02e4232d1622e8c
EC2 Default User <hrk0911tkym@docomo.ne.jp>
2022-08-31 16:14:13 +0000

    [Add]showページまで作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,23 +2,26 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(list_params)
+    book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -26,7 +29,7 @@ class BooksController < ApplicationController
   end
 
 private
-def list_params
+def book_params
    params.require(:book).permit(:title,:body)
 end
 end
